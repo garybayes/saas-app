834649ed8623426817347e087d1f26529b7c2b85
Gary Bayes <garygbayes@gmail.com>
2025-11-19 21:47:44 -0400

    fix: Prisma seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 // prisma/seed.ts
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
-import { encrypt } from "../lib/crypto.ts";
+import { encrypt } from "../src/lib/crypto.ts";
 
 const prisma = new PrismaClient();
 
