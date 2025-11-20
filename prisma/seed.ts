e45689b9ce1ec720e5a6e14e25edfb09e6fb1520
Gary Bayes <garygbayes@gmail.com>
2025-11-19 21:23:49 -0400

    fix: make Prisma seed independent from Next.js crypto module

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 // prisma/seed.ts
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
-import { encrypt } from "../src/lib/crypto";
+import { encrypt } from "./lib/crypto";
 
 const prisma = new PrismaClient();
 
